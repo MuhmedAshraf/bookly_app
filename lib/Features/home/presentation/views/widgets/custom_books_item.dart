c6aa55e2cf69e1092859cc09029d19d457435c8b
MuhmedAshraf <100963016+MuhmedAshraf@users.noreply.github.com>
2023-09-03 22:55:39 +0300

    listviewBooksv

---
+++ lib/Features/home/presentation/views/widgets/custom_books_item.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../Core/utlis/assets.dart';
+
+class CustomBooksItem extends StatelessWidget {
+  const CustomBooksItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.red,
+            image: const DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(AssetsData.testBook),
+            )),
+      ),
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'custom_app_bar.dart';
-import 'custom_book_item.dart';
+import 'custom_books_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({Key? key}) : super(key: key);
@@ -10,9 +10,27 @@ class HomeViewBody extends StatelessWidget {
     return Column(
       children: const [
         CustomAppBar(),
-        CustomBookItem(),
+        ListViewBooks(),
       ],
     );
   }
 }
 
+class ListViewBooks extends StatelessWidget {
+  const ListViewBooks({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: CustomBooksItem(),
+            );
+          }),
+    );
+  }
+}
